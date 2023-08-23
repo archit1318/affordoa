c91fc3a144fc8e8c0948f854aea8f5a15dddd1ed
archit1318 <archittiwari7013@gmail.com>
2023-08-23 11:55:06 +0530

    commi

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import './component/numbers'
+
+import React, {useEffect, useState} from 'react';
+import { response } from 'express';
+
+function number(){
+  const [numbers, setNumbers] = useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:8008/numbers').then(response=>{
+      setNumbers(response.data.numbers);
+    }).then(data=>{
+      SVGMetadataElement(data);
+    }).catch(error => {
+      console.error(error);
+    });
+  }, []);
+
+}
+return (
+  <div>
+    numbers.map(number (key={number}));
+  </div>
+)
+function App() {
+  return (
+    <div className="App">
+      <number/>
+    </div>
+  );
+}
+
+
+export default App;
